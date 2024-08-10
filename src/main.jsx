ae62e41e7a38926e99e29f14063846067cf3ba35
farukh110 <farukhsajjad110@gmail.com>
2024-08-10 16:10:46 +0500

    updated few things

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
