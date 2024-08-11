bd8b05d8c30abb9d3a9e151b62c5b6b8041f5b9c
farukh110 <farukhsajjad110@gmail.com>
2024-08-11 16:16:11 +0500

    Installed sass

--- src/scenes/auth/login/index.jsx
+++ src/scenes/auth/login/index.jsx
@@ -1,6 +1,36 @@
+import './index.scss';
+
 const Login = () => {
     return (
-        <div>Login</div>
+        <>
+            <div className="login-page">
+
+                <div className='row'>
+
+                    <div className='col-md-6 login-left-area'>
+
+                    </div>
+
+                    <div className='col-md-6'>
+
+                        <div className='container'>
+
+                            <div className='row'>
+
+                                <div className='col-md-12'>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </>
     )
 }
 
