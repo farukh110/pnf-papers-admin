fe17328f8c3fa7e05d402c32bdb38fff16cd0e47
farukh110 <farukhsajjad110@gmail.com>
2024-08-15 23:09:05 +0500

    installed new package

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -9,7 +9,67 @@ const Dashboard = () => {
 
                     <div className='col-md-4'>
 
-                        <div className='dashboard-card bg-white rounded-3 shadow p-3'>
+                        <div className='dashboard-card card-light-gray rounded-3 p-3'>
+
+                            <div className='row'>
+
+                                <div className='col-md-6'>
+
+                                    <p className='mb-0'>
+                                        Total
+                                    </p>
+                                    <h4 className='mb-0'> 500 </h4>
+                                </div>
+
+                                <div className='col-md-6 align-self-end'>
+
+                                    <h6 className='mb-0 float-end'> <BsArrowDownRight className='text-success' /> 40% </h6>
+                                    <br />
+                                    <p className='mb-0 float-end'>
+                                        Compare to August 2024
+                                    </p>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className='col-md-4'>
+
+                        <div className='dashboard-card card-light-green rounded-3 p-3'>
+
+                            <div className='row'>
+
+                                <div className='col-md-6'>
+
+                                    <p className='mb-0'>
+                                        Total
+                                    </p>
+                                    <h4 className='mb-0'> 500 </h4>
+                                </div>
+
+                                <div className='col-md-6 align-self-end'>
+
+                                    <h6 className='mb-0 float-end'> <BsArrowDownRight className='text-success' /> 40% </h6>
+                                    <br />
+                                    <p className='mb-0 float-end'>
+                                        Compare to August 2024
+                                    </p>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className='col-md-4'>
+
+                        <div className='dashboard-card card-light-sky rounded-3 p-3'>
 
                             <div className='row'>
 
