b5cd8d6bf95e9f05fc01386b403fc56e41265c0d
farukh110 <farukhsajjad110@gmail.com>
2024-08-17 23:03:45 +0500

    i created add blog form

--- src/scenes/blogs/AddBlog.jsx
+++ src/scenes/blogs/AddBlog.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import { Button, Form, Input, Select } from "antd";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { InboxOutlined } from '@ant-design/icons';
+import { message, Upload } from 'antd';
+const { Dragger } = Upload;
+import './index.scss';
 
 const AddBlog = () => {
 
@@ -21,8 +25,29 @@ const AddBlog = () => {
     const handleDescription = (e) => {
 
         console.log('description: ', e);
+        setDescription(e);
 
     }
+
+    const props = {
+        name: 'file',
+        multiple: true,
+        action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+        onChange(info) {
+            const { status } = info.file;
+            if (status !== 'uploading') {
+                console.log(info.file, info.fileList);
+            }
+            if (status === 'done') {
+                message.success(`${info.file.name} file uploaded successfully.`);
+            } else if (status === 'error') {
+                message.error(`${info.file.name} file upload failed.`);
+            }
+        },
+        onDrop(e) {
+            console.log('Dropped files', e.dataTransfer.files);
+        },
+    };
 
     return (
         <>
@@ -31,6 +56,7 @@ const AddBlog = () => {
                 <h4 className='mt-md-2'> Add Blog </h4>
 
                 <div className='col-md-12'>
+
                     <Form
                         className="mt-md-3"
                         onFinish={onFinish}
@@ -76,7 +102,6 @@ const AddBlog = () => {
                                 >
 
                                     <Select
-                                        defaultValue="lucy"
                                         onChange={handleChange}
                                         options={[
                                             {
@@ -97,7 +122,7 @@ const AddBlog = () => {
                                 </Form.Item>
                             </div>
 
-                            <div className="col-md-12">
+                            <div className="col-md-12 mt-md-2">
                                 <label>
                                     Blog Description
                                 </label>
@@ -116,6 +141,7 @@ const AddBlog = () => {
                                     <ReactQuill
                                         theme="snow"
                                         value={description}
+                                        className="custom-editor"
                                         onChange={(e) => handleDescription(e)}
                                     />
 
@@ -123,13 +149,39 @@ const AddBlog = () => {
 
                             </div>
 
+                            <div className="col-md-4">
+                                <Form.Item
+                                    name="upload_images"
+                                    className="mt-md-4"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please upload images',
+                                        },
+                                    ]}
+                                >
+
+                                    <Dragger {...props}>
+                                        <p className="ant-upload-drag-icon">
+                                            <InboxOutlined />
+                                        </p>
+                                        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                        <p className="ant-upload-hint">
+                                            Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+                                            banned files.
+                                        </p>
+                                    </Dragger>
+
+                                </Form.Item>
+                            </div>
+
                         </div>
 
-                        <div className="row">
+                        <div className="row mt-md-3">
 
                             <Form.Item>
                                 <Button type="primary" htmlType="submit">
-                                    Submit
+                                    Add Blog
                                 </Button>
                             </Form.Item>
 
