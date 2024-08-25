e0dee651af09ed45f2941e62c4c72b6a4e34f47c
farukh110 <farukhsajjad110@gmail.com>
2024-08-25 12:16:26 +0500

    I have created custom progress bar component

---
+++ src/components/global/custom-web-controls/custom-progress-bar/index.jsx
@@ -0,0 +1,11 @@
+import { ProgressBar } from "primereact/progressbar";
+
+const CustomProgressBar = (props) => {
+    return (
+        <>
+            <ProgressBar {...props} />
+        </>
+    );
+};
+
+export default CustomProgressBar;
