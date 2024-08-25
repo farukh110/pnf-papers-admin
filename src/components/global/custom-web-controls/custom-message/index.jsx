2925fe9863d2ec335bf9bf6dc72d419fae1e1103
farukh110 <farukhsajjad110@gmail.com>
2024-08-25 12:19:31 +0500

    I have created custom message component

---
+++ src/components/global/custom-web-controls/custom-message/index.jsx
@@ -0,0 +1,11 @@
+import { Message } from "primereact/message";
+
+const CustomMessage = (props) => {
+    return (
+        <>
+            <Message {...props} />
+        </>
+    );
+};
+
+export default CustomMessage;
