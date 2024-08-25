1cb7db9c505b01999e924a6248ec63739bfbf128
farukh110 <farukhsajjad110@gmail.com>
2024-08-25 12:15:38 +0500

    I have created custom spinner component

---
+++ src/components/global/custom-web-controls/custom-spinner/index.jsx
@@ -0,0 +1,11 @@
+import { ProgressSpinner } from "primereact/progressspinner";
+
+const CustomSpinner = (props) => {
+    return (
+        <>
+            <ProgressSpinner {...props} />
+        </>
+    );
+};
+
+export default CustomSpinner;
