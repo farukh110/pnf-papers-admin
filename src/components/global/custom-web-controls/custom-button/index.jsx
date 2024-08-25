125cab9007010ed8bcc453e9d0a58df246b608dc
farukh110 <farukhsajjad110@gmail.com>
2024-08-25 12:02:15 +0500

    I have created a generic button component

---
+++ src/components/global/custom-web-controls/custom-button/index.jsx
@@ -0,0 +1,11 @@
+import { Button } from "primereact/button";
+
+const CustomButton = (props) => {
+    return (
+        <>
+            <Button {...props} />
+        </>
+    );
+};
+
+export default CustomButton;
