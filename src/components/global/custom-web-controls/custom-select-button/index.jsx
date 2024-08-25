0393d65e5a1dd805e005bd5f64b1b414a578dc79
farukh110 <farukhsajjad110@gmail.com>
2024-08-25 14:29:43 +0500

    I have created custom select button component

---
+++ src/components/global/custom-web-controls/custom-select-button/index.jsx
@@ -0,0 +1,12 @@
+import { SelectButton } from "primereact/selectbutton";
+import './index.scss';
+
+const CustomSelectButton = (props) => {
+    return (
+        <>
+            <SelectButton {...props} />
+        </>
+    );
+};
+
+export default CustomSelectButton;
