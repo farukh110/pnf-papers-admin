790e9b8b8d00afc9a5a648fe18a1cfc8f147a7d0
farukh110 <farukhsajjad110@gmail.com>
2024-08-25 12:13:40 +0500

    I have created custom tooltip component

---
+++ src/components/global/custom-web-controls/custom-tooltip/index.jsx
@@ -0,0 +1,12 @@
+import { Tooltip } from "primereact/tooltip";
+import './index.scss';
+
+const CustomTooltip = (props) => {
+    return (
+        <>
+            <Tooltip {...props} />
+        </>
+    );
+};
+
+export default CustomTooltip;
