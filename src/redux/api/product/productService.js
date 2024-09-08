dfe46b7701755dd4f927f99ebbca635e3facb9e8
farukh110 <farukhsajjad110@gmail.com>
2024-09-08 17:10:55 +0500

    i have updated few things

--- src/redux/api/product/productService.js
+++ src/redux/api/product/productService.js
@@ -1,10 +1,23 @@
 import axios from "axios";
 import { BACKEND } from "../../utilities/base_url";
 
-const getAllProducts = async () => {
+const getAllProducts = async (params) => {
 
-    const response = await axios.get(`${BACKEND}/api/product?`);
-    return response;
+    const { page, limit, sortBy, sortOrder, filters } = params;
+
+    const queryString = new URLSearchParams({
+        page,
+        limit,
+        sortBy,
+        sortOrder,
+        filters: JSON.stringify(filters), // Properly stringify the filters
+    }).toString();
+
+    const response = await axios.get(`${BACKEND}/api/product?${queryString}`);
+
+    if (response.data) {
+        return response.data;
+    }
 };
 
 const productService = {
