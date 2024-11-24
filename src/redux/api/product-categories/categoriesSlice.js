e3fa8054fa51aca4abeb2491e089fef06f2c0f6e
farukh110 <farukhsajjad110@gmail.com>
2024-11-24 17:33:02 +0500

    updated few things

--- src/redux/api/product-categories/categoriesSlice.js
+++ src/redux/api/product-categories/categoriesSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { GET_ALL_PRODUCTS_CATEGORY } from "../../../app-constants";
+import { GET_ALL_PRODUCTS_CATEGORY, GET_ALL_PRODUCTS_CATEGORY_OPTION } from "../../../app-constants";
 import categoriesService from "./categoriesService";
 
 const initialState = {
@@ -27,6 +27,20 @@ export const getAllProductsCategory = createAsyncThunk(GET_ALL_PRODUCTS_CATEGORY
 
 });
 
+export const getAllCategoryOption = createAsyncThunk(GET_ALL_PRODUCTS_CATEGORY_OPTION, async (thunkAPI) => {
+
+    try {
+
+        const response = categoriesService.getAllCategoryOption();
+        return response;
+
+    } catch (error) {
+
+        return thunkAPI.rejectWithValue(error?.response?.data || error?.message);
+    }
+
+});
+
 export const categoriesSlice = createSlice({
 
     name: "productsCategory",
@@ -35,6 +49,7 @@ export const categoriesSlice = createSlice({
     extraReducers: (builder) => {
 
         builder
+            // get all products category
             .addCase(getAllProductsCategory.pending, (state) => {
 
                 state.isLoading = true;
@@ -54,7 +69,29 @@ export const categoriesSlice = createSlice({
                 state.isSuccess = false;
                 state.message = action.payload;
 
-            });
+            })
+            // get all products category option
+            .addCase(getAllCategoryOption.pending, (state) => {
+
+                state.isLoading = true;
+
+            })
+            .addCase(getAllCategoryOption.fulfilled, (state, action) => {
+
+                state.isLoading = false;
+                state.isError = false;
+                state.isSuccess = true;
+                state.categories = action.payload;
+
+            })
+            .addCase(getAllCategoryOption.rejected, (state, action) => {
+
+                state.isLoading = false;
+                state.isError = true;
+                state.isSuccess = false;
+                state.message = action.payload;
+
+            })
     }
 
 });
