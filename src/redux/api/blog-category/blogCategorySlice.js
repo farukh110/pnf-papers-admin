2e9b380b433e89341afbb481ff528984c499d3ba
farukh110 <farukhsajjad110@gmail.com>
2025-01-14 15:57:29 +0500

    I have created slice api function

--- src/redux/api/blog-category/blogCategorySlice.js
+++ src/redux/api/blog-category/blogCategorySlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { CREATE_BLOG_CATEGORY, GET_ALL_BLOGS_CATEGORY } from "../../../app-constants";
+import { CREATE_BLOG_CATEGORY, GET_ALL_BLOGS_CATEGORY, GET_ALL_BLOGS_CATEGORY_OPTION } from "../../../app-constants";
 import blogCategoryService from "./blogCategoryService";
 
 const initialState = {
@@ -19,6 +19,20 @@ export const getAllBlogCategories = createAsyncThunk(GET_ALL_BLOGS_CATEGORY, asy
     try {
 
         const response = await blogCategoryService.getAllBlogCategories(params);
+        return response;
+
+    } catch (error) {
+
+        return thunkAPI.rejectWithValue(error?.response?.data || error.message);
+    }
+
+});
+
+export const getAllBlogCategoryOption = createAsyncThunk(GET_ALL_BLOGS_CATEGORY_OPTION, async (thunkAPI) => {
+
+    try {
+
+        const response = await blogCategoryService.getAllBlogCategoryOption();
         return response;
 
     } catch (error) {
@@ -71,6 +85,26 @@ export const blogCategorySlice = createSlice({
                 state.message = action.payload;
 
             })
+            // get all blog category option
+            .addCase(getAllBlogCategoryOption.pending, (state) => {
+
+                state.isLoading = true;
+
+            })
+            .addCase(getAllBlogCategoryOption.fulfilled, (state, action) => {
+
+                state.isLoading = false;
+                state.isError = false;
+                state.isSuccess = true;
+                state.blogCategory = action.payload;
+            })
+            .addCase(getAllBlogCategoryOption.rejected, (state, action) => {
+
+                state.isLoading = false;
+                state.isError = true;
+                state.isSuccess = false;
+                state.message = action.payload;
+            })
             // create category
             .addCase(createBlogCategory.pending, (state) => {
 
@@ -91,9 +125,7 @@ export const blogCategorySlice = createSlice({
                 state.isError = true;
                 state.isSuccess = false;
                 state.message = action.error;
-
             })
-
     }
 });
 
