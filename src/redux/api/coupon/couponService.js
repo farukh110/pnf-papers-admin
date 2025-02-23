0735b15bb2da1fa5fd0f7ddb8ce77c863a76cc75
farukh110 <farukhsajjad110@gmail.com>
2025-02-23 18:08:41 +0500

    created new api slice functions

--- src/redux/api/coupon/couponService.js
+++ src/redux/api/coupon/couponService.js
@@ -22,16 +22,46 @@ const getAllCoupons = async (params) => {
     }
 }
 
+const getCoupon = async (id) => {
+
+    try {
+
+        const response = await axios.get(`${BACKEND}/coupon/${id}`, config);
+        return response.data;
+
+    } catch (error) {
+
+        console.error('Error:', error);
+        throw new Error(error.response?.data?.message || 'api error');
+    }
+}
+
 const createCoupon = async (coupon) => {
 
     const response = await axios.post(`${BACKEND}/coupon`, coupon, config);
     return response.data;
 }
 
+const updateCoupon = async (coupon) => {
+
+    const response = await axios.put(`${BACKEND}/coupon/${coupon.id}`, { name: coupon.name, expiry: coupon.expiry, discount: coupon.discount }, config);
+    return response.data;
+}
+
+const deleteCoupon = async (couponId) => {
+
+    const response = await axios.delete(`${BACKEND}/coupon/${couponId}`, config);
+    return response.data;
+}
+
+
 const couponService = {
 
     getAllCoupons,
-    createCoupon
+    getCoupon,
+    createCoupon,
+    updateCoupon,
+    deleteCoupon
 }
 
 export default couponService;
